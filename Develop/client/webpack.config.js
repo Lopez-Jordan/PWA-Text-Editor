b0b7d9d87752b94dafa8b81134bedcdfc8f79a72
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-27 22:11:10 -0700

    finished the config.js

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,6 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');  // U DONT NEED THIS TECHNICALLY
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.
@@ -18,12 +19,48 @@ module.exports = () => {
       path: path.resolve(__dirname, 'dist'),
     },
     plugins: [
-      
+      new HtmlWebpackPlugin({
+        title: 'JATE Homepage',
+        template: './index.html',
+    }),
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js'
+      }),
+      new WebpackPWAManifest({  //////////////////////////////////////////////////////////////////////////////////////
+        name: 'TODOs',            //CHANGE ALL HERE
+        short_name: 'TODOs',
+        description: 'Keep track of important tasks!',
+        background_color: '#7eb4e2',
+        theme_color: '#7eb4e2',
+        start_url: './',
+        publicPath: './',
+        icons: [
+        {
+            src: path.resolve('assets/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+        },
+        ],
+      }),     //////////////////////////////////////////////////////////////////////////////////////
     ],
 
     module: {
       rules: [
-        
+        {
+          test: /\.css$/i,
+          use: ['style-loader', 'css-loader'],
+        },
+        {
+          test: /\.m?js$/,
+          exclude: /(node_modules|bower_components)/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        },
       ],
     },
   };
