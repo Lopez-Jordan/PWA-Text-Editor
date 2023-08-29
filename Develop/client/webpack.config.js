842ea5c88362a5c704a89bdf98f56fad3e443efe
Jordan Lopez <jordanlopezemail@gmail.com>
2023-08-28 17:56:34 -0700

    service worker has been properlly installed and registered

--- Develop/client/webpack.config.js
+++ Develop/client/webpack.config.js
@@ -1,7 +1,7 @@
 const HtmlWebpackPlugin = require('html-webpack-plugin');
-const WebpackPwaManifest = require('webpack-pwa-manifest');
+// const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
-const MiniCssExtractPlugin = require('mini-css-extract-plugin');  // U DONT NEED THIS TECHNICALLY
+
 const { InjectManifest } = require('workbox-webpack-plugin');
 
 // TODO: Add and configure workbox plugins for a service worker and manifest file.  DONE
@@ -21,27 +21,27 @@ module.exports = () => {
       new HtmlWebpackPlugin({
         title: 'JATE Homepage',
         template: './index.html',
-    }),
+      }),
       new InjectManifest({
         swSrc: './src-sw.js',
         swDest: 'src-sw.js'
       }),
-      new WebpackPWAManifest({  //////////////////////////////////////////////////////////////////////////////////////
-        name: 'TODOs',            //CHANGE ALL HERE
-        short_name: 'TODOs',
-        description: 'Keep track of important tasks!',
-        background_color: '#7eb4e2',
-        theme_color: '#7eb4e2',
-        start_url: './',
-        publicPath: './',
-        icons: [
-        {
-            src: path.resolve('assets/images/logo.png'),
-            sizes: [96, 128, 192, 256, 384, 512],
-            destination: path.join('assets', 'icons'),
-        },
-        ],
-      }),     //////////////////////////////////////////////////////////////////////////////////////
+      // new WebpackPWAManifest({  //////////////////////////////////////////////////////////////////////////////////////
+      //   name: 'TODOs',            //CHANGE ALL HERE
+      //   short_name: 'TODOs',
+      //   description: 'Keep track of important tasks!',
+      //   background_color: '#7eb4e2',
+      //   theme_color: '#7eb4e2',
+      //   start_url: './',
+      //   publicPath: './',
+      //   icons: [
+      //   {
+      //       src: path.resolve('assets/images/logo.png'),
+      //       sizes: [96, 128, 192, 256, 384, 512],
+      //       destination: path.join('assets', 'icons'),
+      //   },
+      //   ],
+      // }),     //////////////////////////////////////////////////////////////////////////////////////
     ],
 
     module: {
